10bdb301851c45c1ebe7d94e012c2953eee0d923
Elaine Cristina Rocha de Oliveira <Elaine Cristina Rocha de Oliveira>
2016-04-12 23:16:45 -0300

    Criando aplicação Ntalk, acompanhando pelo livro

---
+++ ntalk/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var app = express();
+
+// view engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname, 'public'));
+
+app.use('/', routes);
+app.use('/usuarios', users);
+
+app.listen(3000, function(){
+  console.log("Ntalk no ar.");
+});
